873f692a149a6d8492bc514c566cd747fd12771f
yongjie.xu <xuyongjie1128@hotmail.com>
2016-03-14 22:37:08 +0800

    add QAClient api

--- QA.Client/QAClient.cs
+++ QA.Client/QAClient.cs
@@ -10,6 +10,12 @@ namespace QA.Client
     {
         private const string QuestionPrefix = "api/Questions/";
         private const string AnswerPrefix = "api/Answers/";
+		
+		private const string GetAllQuestions="all";
+		private const string GetAllTimeLineQuestions="timeline";
+		private const string CreateQuestion="";
+		
+		private const string CreateAnswer="";
 
         public HttpClient HttpClient { get; private set; }
         private bool disposed;
@@ -21,6 +27,48 @@ namespace QA.Client
             }
             this.HttpClient = client;
         }
+		
+		
+		
+		public async Task<HttpResult<List<QuestionDTO>>> GetAllQuestionsAsync()
+        {
+            ThrowIfDisposed();
+            HttpResult<List<QuestionDTO>> result = await HttpClient.GetAsync<List<QuestionDTO>>(GetAllQuestions);
+            return result;
+        }
+		
+		public async Task<HttpResult<List<QuestionDTO>>> GetTimeLineAllQuestionsAsync()
+        {
+            ThrowIfDisposed();
+            HttpResult<List<QuestionDTO>> result = await HttpClient.GetAsync<List<QuestionDTO>>(GetAllTimeLineQuestions);
+            return result;
+        }
+
+        public async Task<HttpResult<QuestionDetailDTO>> GetQuestionDetailByQuestionIdAsync(int questionId)
+        {
+            ThrowIfDisposed();
+            HttpResult<QuestionDetailDTO> result = await HttpClient.GetAsync<QuestionDetailDTO>(QuestionPrefix+questionId.ToString());
+            return result;
+        }
+
+        public async Task<HttpResult<QuestionDTO>> PostQuestionAsync(Question question)
+        {
+            ThrowIfDisposed();
+            HttpResult<QuestionDTO> result = await HttpClient.PostAsJsonAsync<Question, QuestionDTO>(CreateQuestion, question);
+            return result;
+        }
+
+		
+		
+		
+        public async Task<HttpResult<AnswerDTO>> PostAnswerAsync(Answer answer)
+        {
+            ThrowIfDisposed();
+            HttpResult<AnswerDTO> result = await HttpClient.PostAsJsonAsync<Answer, AnswerDTO>(CreateAnswer, answer);
+            return result;
+        }
+		
+		
         public void Dispose()
         {
             if(!disposed)
